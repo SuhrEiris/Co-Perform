bbdfe19e8fdda6cc1bd42d7b5cf465054db0c4b9
nh91 <nasuh@dtu.dk>
2022-05-10 10:12:56 +0200

    Grid.arrange for multipanel figures

--- gridarrange.R
+++ gridarrange.R
@@ -6,3 +6,12 @@ ggsave("Fig3ABCD.pdf",
        width=15,
        height=15,
        dpi = 300)
+
+FIGS2ABCD = grid.arrange(PS4A, PS4B, PS4C, noave_2, ncol = 2, nrow=2)
+
+ggsave("FigS2ABCD.pdf",
+       plot = FIGS2ABCD,
+       units="cm",
+       width=18,
+       height=15,
+       dpi = 300)
